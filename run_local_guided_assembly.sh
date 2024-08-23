8bb7c23b921290b1d67de51fd1d2542b72199aca
Bionano Development <dev@bionanogenomics.com>
2024-08-23 13:16:25 -0700

    Update run_local_guided_assembly.sh
    Added description on how to increase the number of compute iteration for CI generation, and that is for cases with a large number of repeat units, beyond the default limit of 5000 repeats.

--- run_local_guided_assembly.sh
+++ run_local_guided_assembly.sh
@@ -11,6 +11,11 @@ export DRMAA_LIBRARY_PATH=/home/sge/lib/lx-amd64/libdrmaa.so
 #   OPTARGS
 #   CLUSTER_ARGS
 
+### Note, if the suspected repeat expansion is beyond 5000 repeats, then please increase the iter_num parameter in the following file in the Solve package:
+### EnFocus_Repeats/1.0/repeat_bayes_model.py
+### the line to change is: def build_probs_r_given_o(df_err, c_ir, obs_inter, unit_size, prior_dist="uniform", prior_mean=None, prior_std=None, exp_copies=-1, iter_num=5000):
+### For example, change the iter_num=9000, if one suspects that the number of repeat to be above 5000 units but less than 9000 units
+
 INPUT_BNX=$1
 GENE=$2
 
